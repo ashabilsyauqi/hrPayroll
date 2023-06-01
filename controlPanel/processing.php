26c62173fe4e9f28361e15620bb331c0ee6f4c71
ashabilsyauqi <ashabilsyauqi@gmail.com>
2023-06-01 12:38:48 +0700

    no message

---
+++ controlPanel/processing.php
@@ -0,0 +1,11 @@
+<?php
+
+    $listJabatan = ['Developer','Manager','Staff','Selles','Marketing'];
+
+
+    $fileDataKaryawan = '../data/dataKaryawan.json';
+    $fileDataAbnsensi = '../data/dataAbsen.json';
+
+    
+
+?>
